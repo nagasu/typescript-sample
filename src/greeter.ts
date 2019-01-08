8387c84e04f61b7cb4266e51297c839d5c95a643
Satoshi Nagasu <s-nagasu.03@nexsimo.co.jp>
2019-01-08 18:25:52 +0900

    Initial commit

---
+++ src/greeter.ts
@@ -0,0 +1,11 @@
+function greeter(person: string) {
+    return "Hello, " + person;
+}
+
+let user = "Jane User";
+console.log(greeter(user));
+
+const a: number = 3;
+const b: string = "abc";
+const c: null = null;
+const d: string = "test";
